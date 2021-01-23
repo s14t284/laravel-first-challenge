5aba63d23c7f484c920ac31b3e4957a00e8d35a9
Ryuya Ikeda <rikeda71@gmail.com>
2021-01-23 20:50:55 +0900

    add view php file

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,12 @@ Route::get('/', function () {
 
 // Controller の利用
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
+// Route::get('hello', 'App\Http\Controllers\HelloController@index');
+// Route::get('hello', function () {
+//     return view('hello.index');
+// });
+Route::get('hello', 'App\Http\Controllers\HelloController@indexWithQueryString');
+Route::get('hello/{id?}', 'App\Http\Controllers\HelloController@index');
 Route::get('hello/other', 'App\Http\Controllers\HelloController@other');
 Route::get('/hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@indexWithRouteParams');
 Route::get('single', 'App\Http\Controllers\SingleActionController');
